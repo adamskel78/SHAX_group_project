fe26db4e2560d81cdb044623320860a3d0108301
Adam Skelton <adamskel78@Adams-MacBook-Pro.local>
2020-08-14 20:45:42 -0500

    Changed the schema again.

--- schema.sql
+++ schema.sql
@@ -1,15 +1,22 @@
-CREATE TABLE review_id_table (
+CREATE TABLE cardio_table (
   id INTEGER,
-  age INTEGER,
+  age varchar,
   gender INTEGER,
   height INTEGER,
-  weight_patient INTEGER,
+  weight_patient varchar,
   ap_hi INTEGER,
   ap_lo INTEGER,
   cholesterol INTEGER,
   gluc INTEGER,
-  smoke INTEGER
+  smoke INTEGER,
   alco INTEGER,
   active INTEGER,
   cardio INTEGER
 );
+
+Select * From cardio_table;
+drop table cardio_table;
+
+copy cardio_table
+From '/Users/adamskel78/Desktop/Data_Analyst/SHAX_group_project/Resources/cardio_train.csv'
+DELIMITER ';' CSV HEADER; 
