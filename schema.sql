9d3531723b09548d11b83609e465df050e3b3b7c
Adam Skelton <adamskel78@Adams-MacBook-Pro.local>
2020-08-14 20:43:55 -0500

    Modified Schema.

---
+++ schema.sql
@@ -0,0 +1,15 @@
+CREATE TABLE review_id_table (
+  id INTEGER,
+  age INTEGER,
+  gender INTEGER,
+  height INTEGER,
+  weight_patient INTEGER,
+  ap_hi INTEGER,
+  ap_lo INTEGER,
+  cholesterol INTEGER,
+  gluc INTEGER,
+  smoke INTEGER
+  alco INTEGER,
+  active INTEGER,
+  cardio INTEGER
+);
